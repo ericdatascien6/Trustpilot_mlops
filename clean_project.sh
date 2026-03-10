bedded9d7bf0982ae0cdef136006acb2e0df2022
ericdatascien6 <eric.datascien6@gmail.com>
2026-03-10 14:33:50 +0000

    commentaire

--- clean_project.sh
+++ clean_project.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 echo "Stopping containers..."
-docker compose down
+docker-compose down
 
 echo "Cleaning Airflow logs..."
 sudo rm -rf airflow/logs/*
-sudo mkdir -p airflow/logs
+mkdir -p airflow/logs
 sudo chown -R 50000:50000 airflow/logs
+
+echo "Resetting Airflow DB..."
+sudo rm -f airflow/airflow.db
 
 echo "Cleaning MLflow runs..."
 sudo rm -rf mlruns/*
 mkdir -p mlruns
 
 echo "Cleaning Python cache..."
-find . -type d -name "__pycache__" -exec rm -rf {} +
+sudo find . -type d -name "__pycache__" -exec rm -rf {} +
 
 echo "Cleanup finished."
